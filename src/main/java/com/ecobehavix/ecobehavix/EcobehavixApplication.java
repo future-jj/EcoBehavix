08d73b66dd4178aa7c646685875f2612b0269a08
future <44869225+future-jj@users.noreply.github.com>
2025-02-25 20:40:55 +0800

    init

---
+++ src/main/java/com/ecobehavix/ecobehavix/EcobehavixApplication.java
@@ -0,0 +1,13 @@
+package com.ecobehavix.ecobehavix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcobehavixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcobehavixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecobehavix/ecobehavix/EcobehavixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecobehavix.ecobehavix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcobehavixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
